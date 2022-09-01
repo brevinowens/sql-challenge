120d7128bf2a456f9aa1d7d4df6dfdeedf605b09
Brevin Owens <brevin.owens@gmail.com>
2022-08-31 23:12:14 -0500

    adding schema file

---
+++ EmployeeSQL/schema.sql
@@ -0,0 +1,43 @@
+-- Create Tables with Primary and Foreign Keys
+
+CREATE TABLE titles (
+    title_id VARCHAR NOT NULL PRIMARY KEY,
+    title VARCHAR NOT NULL
+);
+
+CREATE TABLE employees (
+    emp_no VARCHAR NOT NULL PRIMARY KEY,
+    emp_title_id VARCHAR NOT NULL,
+    birth_date DATE,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    sex VARCHAR NOT NULL,
+    hire_date DATE,
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+CREATE TABLE departments (
+    dept_no VARCHAR NOT NULL PRIMARY KEY,
+    dept_name VARCHAR NOT NULL
+);
+
+CREATE TABLE dept_emp (
+    emp_no VARCHAR NOT NULL,
+    dept_no VARCHAR NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE salaries (
+    emp_no VARCHAR NOT NULL,
+    salary VARCHAR NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR NOT NULL,
+	emp_no VARCHAR NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (emp_no,dept_no)
+);
